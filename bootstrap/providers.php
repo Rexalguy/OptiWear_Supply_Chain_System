4fd4b94848bafac447139a83105713df01199170
Aburek Emmanuel <aburekemmanuel@gmail.com>
2025-07-01 10:19:56 +0300

    filament panels

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CustomerPanelProvider::class,
+    App\Providers\Filament\ManufacturerPanelProvider::class,
+    App\Providers\Filament\SupplierPanelProvider::class,
+    App\Providers\Filament\VendorPanelProvider::class,
 ];
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
-            $table->date('date_of_birth');
-            $table->enum('gender', ['male', 'female', 'other']);
+            $table->enum('role', ['customer', 'vendor', 'supplier', 'admin', 'manufacturer'])
+            ->default('customer');
+            $table->date('date_of_birth')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
