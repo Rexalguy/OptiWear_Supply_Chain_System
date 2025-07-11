cebd29d75fc3b946bbd6fdccaadd710366fe8755
Josemiles-ctr <josephotai209@gmail.com>
2025-07-11 11:36:58 +0300

    New footer

--- .history/app/Providers/AppServiceProvider_20250709121215.php
+++ .history/app/Providers/AppServiceProvider_20250709121215.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Filament\View\PanelsRenderHook;
 use Illuminate\Support\ServiceProvider;
-use Filament\Support\Facades\FilamentView;
-use Illuminate\View\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,11 +18,8 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     // In AuthServiceProvider.php
-    public function boot()
-    {
-        FilamentView::registerRenderHook(
-            PanelsRenderHook::FOOTER,
-            fn(): View => view('footer'),
-        );
-    }
+public function boot()
+{
+}
+
 }
---
+++ .history/resources/views/footer.blade_20250711111036.php
@@ -0,0 +1,6 @@
+
+<footer class="sticky bottom-0 left-0 z-20 w-full p-4 shadow flex items-center justify-center bg-steel-blue">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        &copy {{ now()->year }} OptiWear Supply Chain System &middot; <a href="https://laraveldaily.com/about-project">About LaravelDaily</a>
+    </span>
+</footer>
