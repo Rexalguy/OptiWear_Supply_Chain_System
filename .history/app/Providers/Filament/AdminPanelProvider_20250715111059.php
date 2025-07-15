1ee069d5ec957f740828c8ab3fda2a6551c125b0
Victor Kibenge <bulasiokibenge@gmail.com>
2025-07-15 11:14:40 +0300

    after sesssion expired

--- .history/app/Providers/Filament/AdminPanelProvider_20250715111059.php
+++ .history/app/Providers/Filament/AdminPanelProvider_20250715111059.php
@@ -23,10 +23,9 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Widgets\AccountWidget;
+use Widgets\FilamentInfoWidget;
+use Leandrocfe\FilamentApexCharts\FilamentApexChartsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
