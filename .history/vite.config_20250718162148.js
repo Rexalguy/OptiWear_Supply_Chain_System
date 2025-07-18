c04038c03fbf19bd20ae8758081ea1d3bfe2fed0
Victor Kibenge <bulasiokibenge@gmail.com>
2025-07-18 16:23:52 +0300

    vite changes

--- .history/vite.config_20250718162148.js
+++ .history/vite.config_20250718162148.js
@@ -8,9 +8,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/filament/customer/theme.css',
-                'resources/css/filament/admin/theme.css'],
+                 'resources/js/app.js',
+                  'resources/css/filament/customer/theme.css',
+                    'resources/css/filament/admin/theme.css'],
             refresh: true,
         }),
         tailwindcss(),
