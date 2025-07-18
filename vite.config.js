54302f75a6d6cca771177af086f8b2d0d2993d97
Aburek Emmanuel <aburekemmanuel@gmail.com>
2025-07-18 16:26:43 +0300

    removed plugin

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
             input: ['resources/css/app.css',
                  'resources/js/app.js',
                   'resources/css/filament/customer/theme.css',
-                   'resources/js/filament-chart-js-plugins.js',
                     'resources/css/filament/admin/theme.css'],
             refresh: true,
         }),
