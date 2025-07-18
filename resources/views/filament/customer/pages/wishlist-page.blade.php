fabfc5c96d0f6f712c521cab1f347f2ecd2d3b8a
Precious Praise <akatukundapp@gmail.com>
2025-07-18 09:27:00 +0300

    adjusted wishlist button

--- resources/views/filament/customer/pages/wishlist-page.blade.php
+++ resources/views/filament/customer/pages/wishlist-page.blade.php
@@ -105,10 +105,8 @@
 
 
                 {{-- Product Details --}}
-                <h3 class="text-lg font-semibold">{{ $clickedProduct->name }}</h3>
-                <p class="text-sm text-gray-500">SKU: {{ $clickedProduct->sku }}</p>
-                <p class="text-sm ">Price: UGX {{ number_format($clickedProduct->unit_price) }}</p>
-                <p class="text-sm text-gray-600">Available: {{ $clickedProduct->quantity_available }}</p>
+                <h3 class="text-lg ">{{ $clickedProduct->name }}</h3>
+                <p class="text-lg font-bold">Price: UGX {{ number_format($clickedProduct->unit_price) }}</p>
                 <p class="text-sm text-gray-600 mt-2">{{ $clickedProduct->description }}</p>
 
                 {{-- Existing Cart Items for this Product --}}
