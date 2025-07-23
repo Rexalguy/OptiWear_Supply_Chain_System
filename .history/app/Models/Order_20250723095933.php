50f43b4372d522969b93f859607ec57ea411512a
Josemiles-ctr <josephotai209@gmail.com>
2025-07-23 11:28:22 +0300

    Placing the order and order item

---
+++ .history/app/Models/Order_20250723095933.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'status', 
+        'created_by', 
+        'delivery_option', 
+        'expected_delivery_date', 
+        'total', 
+        'rating', 
+        'review',
+        'payment_method', 
+        'delivery_method', 
+        'expected_fulfillment_date', 
+        'decline_reason',
+        'manufacturer_id'
+    ];
+
+    protected $casts = [
+        'total' => 'decimal:2',
+        'expected_delivery_date' => 'date',
+        'expected_fulfillment_date' => 'date',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    
+    public function vendor()
+    {
+        return $this->belongsTo(User::class, 'vendor_id');
+    }
+
+    public function manufacturer() 
+    {
+        return $this->belongsTo(User::class, 'manufacturer_id');
+    }
+}
